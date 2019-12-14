45283eaae99a033083e74f5daf6a8d1d4351cf34
Ingo Hinterding <ingo@hinterding.de>
2019-12-14 14:17:44 +0100

    configurable email

--- scraper.py
+++ scraper.py
@@ -42,13 +42,13 @@ import smtplib
 
 
 
-def check_price(URL,desired_price,email,password):
+def check_price(search_object):
 
   headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
 
-  page = requests.get(URL, headers=headers)
-  soup = BeautifulSoup(page.content, 'html.parser')
+  page = requests.get( search_object['url'] , headers=headers )
+  soup = BeautifulSoup( page.content, 'html.parser' )
 
   title = soup.find(id="productTitle").get_text().strip()
   price_string = soup.find(
@@ -61,34 +61,36 @@ def check_price(URL,desired_price,email,password):
   print ("\n----------------------------\n")
   print(title)
   print(f"Current price is: {price_string}")
-  print(f"Price alarm at: {desired_price}")
+  print(f"Price alarm at: {search_object['price']}")
 
-  if (price <= desired_price):
-    print(f"Sending price alarm to {email}")
-    send_mail( title , price_string , desired_price, URL , email , password )
+  if (price <= search_object["price"]):
+    print(f"Sending price alarm to {search_object['email_to']}")
+    search_object["title"] = title
+    search_object["price_string"] = price_string
+    send_mail( search_object )
   else:
     print("No alarm.")
 
 
 
 # send email 
-def send_mail( title , price , desired_price, URL , email , password):
+def send_mail( search_object ):
   server = smtplib.SMTP('smtp.gmail.com', 587)
   server.ehlo()
   server.starttls()
   server.ehlo()
-  server.login(email, password)
+  server.login(search_object['email_from'], search_object['email_from_password'])
 
-  subject = f'Price alarm: {price} for {title[0:40]}...'
+  subject = f"Price alarm: {search_object['price_string']} for {search_object['title'][0:40]}..."
   body = "A product you're watching has dropped in price:\n\n"
-  body += title + "\n\n"
-  body += f"Current price is: {price} (alarm set at {desired_price})\n\n"
-  body += URL
+  body += search_object['title'] + "\n\n"
+  body += f"Current price is: {search_object['price_string']} (alarm set at {search_object['price']})\n\n"
+  body += search_object['url']
   msg = f"Subject: {subject}\n\n{body}"
   
   server.sendmail(
-    email,
-    email,
+    search_object['email_from'],
+    search_object['email_to'],
     msg.encode("utf8"))
 
   server.quit()
@@ -110,7 +112,17 @@ def load_json():
 
 def scraper(json_data):
   for p in json_data['products']:
-    check_price(p['url'], p['price'],json_data['email'],json_data['password'])
+    search_object = {}
+    search_object['url'] = p['url']
+    search_object['price'] = p['price']
+    search_object['email_from'] = json_data['email_from'];
+    search_object['email_from_password'] = json_data['email_from_password']
+    if "email" in p:
+      search_object['email_to'] = p['email']
+    else:
+      search_object['email_to'] = json_data['email_to_default']
+
+    check_price(search_object)
 
 
 
